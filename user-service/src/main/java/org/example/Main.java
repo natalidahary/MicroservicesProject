749c7564819c36654c2ddc0054b3b730cd157181
natali.dahary <natali.dahary@s.afeka.ac.il>
2024-07-07 20:08:35 +0300

    setup news-service

--- user-service/src/main/java/org/example/Main.java
+++ user-service/src/main/java/org/example/Main.java
@@ -6,7 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Main {
     public static void main(String[] args) {
-        System.setProperty("java.net.preferIPv4Stack", "true");
         SpringApplication.run(Main.class, args);
     }
 }
