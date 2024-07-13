e4eab064d5b38861f4d0352d1574271781fd8e8b
natali.dahary <natali.dahary@s.afeka.ac.il>
2024-07-13 12:48:38 +0300

    adding more parmeters to user model

---
+++ user-service/src/main/java/org/example/model/Category.java
@@ -0,0 +1,16 @@
+package org.example.model;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Category {
+    private String name;
+    private String preference;
+}
